766132eb695f8bbd73ac99d98c4cbaa3bd2b88ab
menhaj <menhaj007@yahoo.com>
2021-04-30 11:18:17 -0400

    commit tree 7c7e95e01f499bfb3a493186418fc01a3fb0ca6e

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,28 @@
-// your Bomb code here!
+import React, {Component} from 'react';
+
+export default class Bomb extends Component {
+
+    constructor(props){
+        super();
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+
+    }
+
+    decrement = () => {
+        const subValue =  this.state.secondsLeft-1;
+        this.setState({
+            secondsLeft: subValue
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.secondsLeft === 0? "Boom!" : this.state.secondsLeft + " seconds left before I go boom!" }
+                
+            </div>
+        )
+    }
+}
